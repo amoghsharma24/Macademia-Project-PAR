e0fd151f49763ce4060d782320960f5b0700ce94
amoghsharma24 <amoghsharma02@gmail.com>
2026-05-22 14:32:57 +1000

    Added the tree waypoint planner package skeleton

---
+++ project_files/src/par_coursework/tree_waypoint_planner/setup.py
@@ -0,0 +1,31 @@
+from glob import glob
+import os
+
+from setuptools import find_packages, setup
+
+
+package_name = 'tree_waypoint_planner'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['README.md']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='RMIT AI Innovation Lab',
+    maintainer_email='your.email@student.rmit.edu.au',
+    description='Tree approach waypoint planner for orchard harvesting demos.',
+    license='RMIT IP - Not for public release',
+    entry_points={
+        'console_scripts': [
+            'fake_tree_publisher_node = tree_waypoint_planner.fake_tree_publisher_node:main',
+            'tree_waypoint_planner_node = tree_waypoint_planner.tree_waypoint_planner_node:main',
+        ],
+    },
+)
