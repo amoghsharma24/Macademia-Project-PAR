fca20dc341d0307e522425b2cf2824b55c90bf0c
Tanay0123 <s4007302@student.rmit.edu.au>
2026-06-07 16:36:46 +1000

    V1 Attempt at: - adding marker publishing for rviz - using localisation for the start - using a Pose parameter as a goal

--- orchard_global_planner/orchard_global_planner/planner_node.py
+++ orchard_global_planner/orchard_global_planner/planner_node.py
@@ -3,7 +3,13 @@ from rclpy.node import Node
 
 from nav_msgs.msg import Path
 from nav_msgs.msg import OccupancyGrid
+from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseStamped
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Point
+
+
+from math import atan2
 
 from ompl import base as ob
 from ompl import geometric as og
@@ -22,12 +28,72 @@ class PlannerNode(Node):
 
         self.map_data = None
 
+        self.current_x = None
+        self.current_y = None
+
+        self.goal_x = None
+        self.goal_y = None
+
+        self.plan_requested = False
+
         self.map_sub = self.create_subscription(
             OccupancyGrid, 
             '/map', 
             self.map_callback,
             10
         )
+
+        self.localisation_sub = self.create_subscription(
+            Odometry,
+            '/odom',
+            self.odom_callback,
+            10
+        )
+
+        self.controller_sub = self.create_subscription(
+            PoseStamped, 
+            '/next_pose',
+            self.goal_callback,
+            10
+        )
+
+        self.marker_pub = self.create_publisher(
+            Marker,
+            '/planned_markers',
+            10
+        )
+
+    def quaternion_to_yaw(self, q):
+
+        siny_cosp = 2.0 * (
+            q.w * q.z +
+            q.x * q.y
+        )
+
+        cosy_cosp = 1.0 - 2.0 * (
+            q.y * q.y +
+            q.z * q.z
+        )
+
+        return atan2(
+            siny_cosp,
+            cosy_cosp
+        )
+    
+    def try_plan(self):
+
+        if self.map_data is None:
+            return
+
+        if self.current_x is None:
+            return
+        
+        if self.goal_x is None:
+            return
+
+        self.plan_requested = False
+
+        self.plan_demo_path()
     
     def map_callback(self, msg): 
         self.map_data = msg
@@ -36,12 +102,33 @@ class PlannerNode(Node):
             f"Received map with resolution: {self.map_data.info.resolution}"
         )
 
-        # only plan once
-        if not hasattr(self, 'planned_once'):
+        self.try_plan()
+        # # only plan once
+        # if not hasattr(self, 'planned_once'):
 
-            self.planned_once = True
+        #     self.planned_once = True
 
-            self.plan_demo_path()
+        #     self.try_plan()
+    
+    def odom_callback(self, msg): 
+        self.current_x = msg.pose.pose.position.x
+        self.current_y = msg.pose.pose.position.y
+
+        q = msg.pose.pose.orientation
+        self.current_yaw = self.quaternion_to_yaw(q)
+
+        self.try_plan()
+
+    def goal_callback(self, msg):
+        self.goal_x = msg.pose.position.x
+        self.goal_y = msg.pose.position.y
+
+        q = msg.pose.pose.orientation
+        self.goal_yaw = self.quaternion_to_yaw(q)
+
+        self.plan_requested = True  
+
+        self.try_plan()
 
     def is_state_valid(self, state):
 
@@ -118,14 +205,14 @@ class PlannerNode(Node):
         ss.setStateValidityChecker(self.is_state_valid)
 
         start = space.allocState()
-        start.setX(0.0)
-        start.setY(0.0)
-        start.setYaw(0.0)
+        start.setX(self.current_x)
+        start.setY(self.current_y)
+        start.setYaw(self.current_yaw)
 
         goal = space.allocState()
-        goal.setX(0.0)
-        goal.setY(1.0)
-        goal.setYaw(0.0)
+        goal.setX(self.goal_x)
+        goal.setY(self.goal_y)
+        goal.setYaw(self.goal_yaw)
 
         print(
             "start valid:",
@@ -155,6 +242,9 @@ class PlannerNode(Node):
             path_msg.header.frame_id = 'map'
 
             solution = ss.getSolutionPath()
+            self.publish_path_markers(solution)
+
+            print(type(solution.getState(0)))
 
             for i in range(solution.getStateCount()):
 
@@ -176,6 +266,40 @@ class PlannerNode(Node):
 
         else:
             self.get_logger().warn('No path found')
+
+    def publish_path_markers(self, solution):
+
+        marker = Marker()
+
+        marker.header.frame_id = "map"
+        marker.header.stamp = self.get_clock().now().to_msg()
+
+        marker.ns = "planned_path"
+        marker.id = 0
+
+        marker.type = Marker.LINE_STRIP
+        marker.action = Marker.ADD
+
+        marker.scale.x = 0.05
+
+        marker.color.a = 1.0
+        marker.color.r = 1.0
+        marker.color.g = 0.0
+        marker.color.b = 0.0
+
+        for i in range(solution.getStateCount()):
+
+            state = solution.getState(i)
+
+            p = Point()
+
+            p.x = state.getX()
+            p.y = state.getY()
+            p.z = 0.0
+
+            marker.points.append(p)
+
+        self.marker_pub.publish(marker)
 
 
 def main(args=None):
