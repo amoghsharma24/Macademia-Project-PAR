9194d5173f3229f8e76fafdd79d7a8876bfda85d
Mr-Pallas-uni <s4005841@student.rmit.edu.au>
2026-06-12 22:39:53 +1000

    Yesterday's refactoring, and fixed up launch file.

--- Macadamia_challange/src/par_coursework/Macadamia_challenge/macadamia_challenge/tree_detection/tree_mapper_node.py
+++ Macadamia_challange/src/par_coursework/Macadamia_challenge/macadamia_challenge/tree_detection/tree_mapper_node.py
@@ -1,29 +1,13 @@
-# 1. load map
-# 2. RANSAC to detect circles.
-# 3. Filter circles (?)
-# 4. Publish circles
-
-# one shot, no history.
-
 import cv2
 import numpy as np
 import rclpy
 from builtin_interfaces.msg import Time
-from geometry_msgs.msg import Pose, PoseArray
+from geometry_msgs.msg import PoseStamped, Pose, PoseArray
 from nav_msgs.msg import OccupancyGrid
 from numpy.typing import NDArray
 from rclpy.node import Node
 from typing import List, Dict, Tuple
-from rclpy.qos import QoSProfile, ReliabilityPolicy, DurabilityPolicy, HistoryPolicy
 from std_msgs.msg import Empty
-
-
-map_qos = QoSProfile(
-    reliability=ReliabilityPolicy.RELIABLE,
-    durability=DurabilityPolicy.TRANSIENT_LOCAL,
-    history=HistoryPolicy.KEEP_LAST,
-    depth=1,
-)
 
 
 class treeMapper(Node):
@@ -34,14 +18,12 @@ class treeMapper(Node):
         self.declare_parameters(
             namespace="",
             parameters=[
-                ("image_path", "."),
-                ("input_map_topic", "/map"),
-                ("output_trees_topic", "/trees"),
-                ("frame_id", "map"),
                 ("max_radius", 1.0),
                 ("min_radius", 1.0),
                 ("free_threshold", 1),
                 ("occupied_threshold", 1),
+                ("height_bounds", 1.0),
+                ("width_bounds", 1.0),
             ],
         )
 
@@ -54,30 +36,18 @@ class treeMapper(Node):
         self.started = False
 
         # Publishers
-        self.tree_publisher = self.create_publisher(
-            PoseArray,
-            self.get_parameter("output_trees_topic").value,
-            10,
-        )
+
+        self.tree_publisher = self.create_publisher(PoseArray, "/trees", 10)
+
         # self.single_tree_publisher = self.create_publisher(Pose, "/tree", 1)
 
-        # TF
-        # self.tf_buffer = tf2_ros.Buffer()
-        # self.tf_listener = tf2_ros.TransformListener(self.tf_buffer, self)
-
         # Timers
-        # self.timer = self.create_timer(10, self.generate_from_topic)
 
-        # Parameters
-        self.map_path = self.get_parameter("image_path").value
+        self.timer = self.create_timer(1, self.generate_from_topic)
 
         # Subscriptions
-        self.create_subscription(
-            OccupancyGrid,
-            self.get_parameter("input_map_topic").value,
-            self.parse_occupancy_msg,
-            map_qos,
-        )
+
+        self.create_subscription(OccupancyGrid, "/map", self.parse_occupancy_msg, 10)
 
         self.create_subscription(
             Empty, "/tree_generator_start", self.run_generation, 10
@@ -85,7 +55,7 @@ class treeMapper(Node):
 
     # region main functions
 
-    def detect_contours(
+    def transform_to_contours(
         self,
         img: NDArray[np.uint8],
     ) -> NDArray[np.float32] or None:
@@ -96,49 +66,47 @@ class treeMapper(Node):
 
         return contours
 
-    def filter_contours(
-        self, contours: NDArray[np.float32]
-    ) -> List[Tuple[float, float]]:
-
-        filtered = []
+    def check_radius(self, r: float) -> bool:
 
         min_radius = self.parameters["min_radius"]
-
         max_radius = self.parameters["max_radius"]
+        if r < min_radius or r > max_radius:
+            return False
+        return True
 
-        for c in contours:
-            # area = cv2.contourArea(c)
+    def check_bounds(self, x: float, y: float) -> bool:
 
-            (x, y), radius = cv2.minEnclosingCircle(c)
+        width = self.parameters["width_bounds"]
+        height = self.parameters["height_bounds"]
+        #   2 * width
+        #      _______
+        #     |       |
+        #     |       |
+        #     |       |
+        #     |       | height
+        #     --[0,0]--
 
-            if radius >= min_radius and radius <= max_radius:
-                # self.publish_circle(x, y, radius)
-                filtered.append((x, y))
+        if abs(x) > width:
+            return False
 
-        return filtered
+        if y > height or y < 0:
+            return False
+
+        return True
 
     # endregion
 
     # region conversion methods
-    def convert_parameters_to_pixels(self, parameters: list):
-        newParams = []
-        for p in parameters:
-            newParams.append(int(p / self.most_recent_resolution))
 
-        return newParams
-
-    def convert_grid_to_image(
+    def transform_grid_to_image(
         self,
         msg: OccupancyGrid,
     ) -> NDArray[np.uint8]:
         """
-
         Convert a ROS2 OccupancyGrid into an OpenCV binary image.
 
-
-
         OccupancyGrid values:
-            -1   = unknown
+            -1  = unknown
             0   = free
             100 = occupied
         """
@@ -151,43 +119,19 @@ class treeMapper(Node):
 
         # Create binary image
 
-        # free      -> 255 (white)
-        # occupied  -> 0   (black)
-
-        # unknown   -> 0 (gray)
+        # free      -> 0   (black)
+        # occupied  -> 255 (white)
+        # unknown   -> 0   (black)
 
         img = np.zeros((height, width), dtype=np.uint8)
 
-        img[(grid <= 25) & (grid >= 0)] = 0
+        free_thresh = self.parameters["free_threshold"]
+        occupied_thresh = self.parameters["occupied_threshold"]
 
-        img[grid >= 65] = 255
-
-        img[grid == -1] = 0
+        img[(grid <= free_thresh)] = 0
+        img[grid >= occupied_thresh] = 255
 
         return img
-
-    def convert_circles_to_map(
-        self,
-        origin: Dict[str, float],
-        resolution: float,
-        circles: NDArray[np.float32],
-    ) -> List[Tuple[float, float]]:
-
-        # 1. convert to correct resolution
-        # 2. convert to map frame
-        # - is positive x forward? is positive y left?
-
-        circles_translated = []
-
-        for x, y in circles:
-            x_meters = x * resolution + origin["origin_x"]
-
-            y_meters = y * resolution + origin["origin_y"]
-
-            circles_translated.append((x_meters, y_meters))
-
-        # self.get_logger().info(f"trees transformed: {circles_translated}")
-        return circles_translated
 
     def get_origin(
         self,
@@ -206,34 +150,16 @@ class treeMapper(Node):
         }
         return metadata
 
-    def generate_trees(
-        self, map_image: NDArray[np.uint8]
-    ) -> List[Tuple[float, float]] or None:
-
-        contours = self.detect_contours(map_image)
-
-        if len(contours) == 0:
-            self.get_logger().info("no trees detected")
-            return None
-
-        trees = self.filter_contours(contours)
-
-        if len(trees) == 0:
-            self.get_logger().info("all trees were filtered out")
-            return None
-
-        self.get_logger().info(f"trees detected: {len(trees)}")
-
-        return trees
-
     # endregion
     # region control methods
-    def run_generation(self, _msg=None) -> None:
+
+    def run_generation(self, msg: Empty) -> None:
         self.started = True
         self.get_logger().info("tree generation request recieved.")
         self.generate_from_topic()
 
     def generate_from_topic(self) -> None:
+
         if not self.started:
             return
 
@@ -244,21 +170,53 @@ class treeMapper(Node):
             or self.most_recent_time is None
             or self.parameters is None
         ):
+            self.get_logger().info(f"""starting information not recieved.
+            map: {self.most_recent_map is not None}
+            origin: {self.most_recent_origin is not None}
+            resolution: {self.most_recent_resolution is not None}
+            time: {self.most_recent_time is not None}
+            parameters: {self.parameters is not None}""")
             return
 
-        circles = self.generate_trees(self.most_recent_map)
-        self.get_logger().info("1/3 trees gotten")
+        self.get_logger().info("starting information & start trigger recieved.")
+        contours = self.transform_to_contours(self.most_recent_map)
 
-        if circles is None:
+        if contours is None or len(contours) == 0:
+            self.get_logger().info("No contours found.")
             return
 
-        transformed_circles = self.convert_circles_to_map(
-            self.most_recent_origin, self.most_recent_resolution, circles
-        )
+        trees = []
+        for c in contours:
+            (x, y), r = cv2.minEnclosingCircle(c)
+            # convert to meters
+            x_meters = (
+                x * self.most_recent_resolution + self.most_recent_origin["origin_x"]
+            )
+            y_meters = (
+                y * self.most_recent_resolution + self.most_recent_origin["origin_y"]
+            )
+            r_meters = r * self.most_recent_resolution
 
-        self.get_logger().info("2/3 circles transformed")
-        self.publish_circles(transformed_circles, self.most_recent_time)
-        self.get_logger().info("3/3 circles published")
+            # if not self.check_bounds(x_meters, y_meters):
+
+            #     continue
+
+            if not self.check_radius(r_meters):
+                continue
+            # self.publish_circle(x_meters, y_meters, r_meters)
+            trees.append((x_meters, y_meters))
+
+        self.get_logger().info("trees processed")
+
+        if len(trees) == 0:
+            self.get_logger().info("all trees were filtered out")
+            return None
+
+        self.get_logger().info(f"trees detected: {len(trees)}")
+
+        self.get_logger().info("2/3 trees transformed")
+        self.publish_circles(trees, self.most_recent_time)
+        self.get_logger().info("3/3 trees published")
 
     # endregion
     # region subscribers
@@ -266,30 +224,31 @@ class treeMapper(Node):
         self,
         msg: OccupancyGrid,
     ) -> None:
+
+        if not self.started:
+            return
+        # self.get_logger().info("grid recieved, processing info")
+
+        self.parameters = self.get_parameters_dict()
         self.most_recent_origin = self.get_origin(msg)
-        self.most_recent_map = self.convert_grid_to_image(msg)
+        self.most_recent_map = self.transform_grid_to_image(msg)
         self.most_recent_resolution = msg.info.resolution
         self.most_recent_time = msg.info.map_load_time
-        self.parameters = self.get_parameters()
+
+        # self.get_logger().info("info processed")
 
     # endregion
 
     # region value grabbers
 
-    def get_parameters(self):
-
-        distance_params = [
-            self.get_parameter("min_radius").value,
-            self.get_parameter("max_radius").value,
-        ]
-
-        scaled_params = self.convert_parameters_to_pixels(distance_params)
-
+    def get_parameters_dict(self):
         params_dict = {
-            "min_radius": scaled_params[0],
-            "max_radius": scaled_params[1],
+            "min_radius": self.get_parameter("min_radius").value,
+            "max_radius": self.get_parameter("max_radius").value,
             "free_threshold": self.get_parameter("free_threshold").value,
-            "occupied_threshold": self.get_parameter("free_threshold").value,
+            "occupied_threshold": self.get_parameter("occupied_threshold").value,
+            "height_bounds": self.get_parameter("height_bounds").value,
+            "width_bounds": self.get_parameter("width_bounds").value,
         }
         return params_dict
 
@@ -301,7 +260,9 @@ class treeMapper(Node):
         time: Time,
     ) -> None:
         trees = PoseArray()
-        trees.header.frame_id = self.get_parameter("frame_id").value
+
+        trees.header.frame_id = "map"
+
         trees.header.stamp = time
 
         # Create the poses
@@ -326,17 +287,17 @@ class treeMapper(Node):
         self.tree_publisher.publish(trees)
 
     def publish_circle(self, x, y, r) -> None:
-        tree = Pose()
+        tree = PoseStamped()
         tree.header.frame_id = "map"
         tree.header.stamp = self.most_recent_time
-        tree.position.x = float(x)
-        tree.position.y = float(y)
-        tree.position.z = 0.0
+        tree.pose.position.x = float(x)
+        tree.pose.position.y = float(y)
+        tree.pose.position.z = 0.0
 
-        tree.orientation.x = 0.0
-        tree.orientation.y = 0.0
-        tree.orientation.z = 0.0
-        tree.orientation.w = 1.0
+        tree.pose.orientation.x = 0.0
+        tree.pose.orientation.y = 0.0
+        tree.pose.orientation.z = 0.0
+        tree.pose.orientation.w = 1.0
 
         # Publish
         self.single_tree_publisher.publish(tree)
